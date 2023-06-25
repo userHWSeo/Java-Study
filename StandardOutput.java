718b94462375c3ba6ba55f394bdd69197668c5e0
useHWSeo <gmldnjseocnd@naver.com>
2023-06-25 15:55:32 +0900

    오버로딩

---
+++ StandardOutput.java
@@ -0,0 +1,24 @@
+public class StandardOutput {
+    public void println(boolean b) {
+        System.out.println(b);
+    }
+    public void println(int i) {
+        System.out.println(i);
+    }
+    public void println(double d) {
+        System.out.println(d);
+    }
+    public void println(String s) {
+        System.out.println(s);
+    }
+
+    // java StandardOutput
+    // Overloading
+    public static void main(String[] args) {
+        StandardOutput output = new StandardOutput();
+        output.println(100);
+        output.println("Hello");
+        output.println(10.5);
+        output.println(false);
+    }
+}
