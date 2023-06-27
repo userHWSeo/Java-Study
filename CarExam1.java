06fd8006f41a555b0d66d4702270a235335813a4
useHWSeo <gmldnjseocnd@naver.com>
2023-06-27 23:01:48 +0900

    상속과 오버라이딩

---
+++ CarExam1.java
@@ -0,0 +1,15 @@
+public class CarExam1 {
+    public static void main(String[] args) {
+        Bus bus1 = new Bus();
+
+        bus1.run();
+        bus1.안내방송();
+
+        Car car1 = new Bus();
+        car1.run();
+
+        Car car2 = new SuperCar();
+        car2.run();
+
+    }
+}
