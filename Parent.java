d527bf6d5097728b155b8d3517e4f958e41daf4e
useHWSeo <gmldnjseocnd@naver.com>
2023-07-03 23:19:01 +0900

    오버라이딩 시 주의할 점과 정보 은닉

--- Parent.java
+++
@@ -1,9 +0,0 @@
-public class Parent {
-    int i = 5;
-    public void printI(){
-        System.out.println("parent - printI() : " + i);
-    }
-    public void printII(){
-        System.out.println(i * 2);
-    }
-}
